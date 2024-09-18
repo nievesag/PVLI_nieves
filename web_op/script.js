bea086b6e99543b30d784eb05a35b8fdafeafae3
nievesag <nievalon@ucm.es>
2024-09-19 00:33:31 +0200

    avances por fin

--- web_op/script.js
+++ web_op/script.js
@@ -1,5 +1,25 @@
-function devuelveTextoDeAlerta() {
-  return "uooooo! Vaya alerta";
+function quadrAlerta() {
+  return "Jimmy: [from the cliff top after deciding not to be a mod, shouts] Me!";
+}
+
+function milagroAlerta() {
+  return "Marcianito 2: [jugando al póker con las cartas del juego de las familias] Chino, china, chinito, negro, negrito. Full de negros chinos.";
+}
+
+function faroAlerta() {
+  return "Thomas Wake : DAMN YE! Let Neptune strike ye dead, Winslow! HAAAAAARK!";
+}
+
+function brokeAlerta() {
+  return "Jack Twist: No more beans.";
+}
+
+function luchaAlerta() {
+  return "Narrator: [reading] I am Jack's colon.";
+}
+
+function clerksAlerta() {
+  return "Caged Animal Masturbator: It's important to have a job that makes a difference, boys. That's why I manually masturbate caged animals for artificial insemination.";
 }
 
 function desaparece(nombre) {
